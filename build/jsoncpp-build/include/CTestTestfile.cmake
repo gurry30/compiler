6001443b95c99c91eeeb3b313ace6eb19fffc542
Wenrry gu <gurrypku@163.com>
2020-06-21 17:40:48 +0800

    v1.3

--- build/jsoncpp-build/include/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/sunnight/CompilerProject-2020Spring-master/3rdparty/jsoncpp/include
-# Build directory: /home/sunnight/CompilerProject-2020Spring-master/build/jsoncpp-build/include
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- build/jsoncpp-build/src/lib_json/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/sunnight/CompilerProject-2020Spring-master/3rdparty/jsoncpp/src/lib_json
-# Build directory: /home/sunnight/CompilerProject-2020Spring-master/build/jsoncpp-build/src/lib_json
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
